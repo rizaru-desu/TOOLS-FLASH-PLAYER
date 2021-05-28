68c39bf4ee45241d0e514da8887fca33f26752de
rizaru-desu <rizaru_desu@yahoo.com>
2021-05-28 14:38:53 +0700

    first commit

---
+++ ToolsFLash/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ToolsFLash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFix = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFix
+            // 
+            this.btnFix.Location = new System.Drawing.Point(12, 109);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(228, 23);
+            this.btnFix.TabIndex = 0;
+            this.btnFix.Text = "PERBAIKI";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(12, 80);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(228, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "PERIKSA COMPONENT";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::ToolsFLash.Properties.Resources.adobe_flash_player_logo_black_and_white;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtConsole.Location = new System.Drawing.Point(12, 139);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.Size = new System.Drawing.Size(228, 95);
+            this.txtConsole.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 246);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnFix);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ALTER FLASH FIX";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtConsole;
+    }
+}
+
---
+++ ToolsFLash/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ToolsFLash
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string comp32 = @"C:\Windows\System32\Macromed\Flash\Flash.ocx";
+            string comp64 = @"C:\Windows\SysWOW64\Macromed\Flash\Flash.ocx";
+
+            if (!File.Exists(comp32) && !File.Exists(comp64))
+            {
+                txtConsole.AppendText("FILE COMPONENT TIDAK DITEMUKAN! \n" + Environment.NewLine);
+            } else
+            {
+                txtConsole.AppendText("FILE COMPONENT DITEMUKAN!\n" + Environment.NewLine);
+            }
+        }
+
+        private void btnFix_Click(object sender, EventArgs e)
+        {
+            string sourceFile32 = Application.StartupPath + @"\include\System32\";
+            string sourceFile64 = Application.StartupPath + @"\include\SysWOW64\";
+            string destinationFile32 = @"C:\Windows\System32\";
+            string destinationFile64 = @"C:\Windows\SysWOW64\";
+
+
+            string sys32 = @"C:\Windows\System32\Macromed\Flash\Flash.ocx";
+            string sys64 = @"C:\Windows\SysWOW64\Macromed\Flash\Flash.ocx";
+            try
+            {
+                FileSystem.CopyDirectory(sourceFile32, destinationFile32, UIOption.AllDialogs);
+                FileSystem.CopyDirectory(sourceFile64, destinationFile64, UIOption.AllDialogs);
+                try
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.UseShellExecute = false;
+                    startInfo.Verb = "runas";
+                    startInfo.RedirectStandardInput = true;
+                    startInfo.RedirectStandardOutput = true;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.StandardInput.WriteLine("regsvr32 " + sys32 + " && " + "regsvr32 " + sys64);
+                    process.StandardInput.Flush();
+                    process.StandardInput.Close();
+                    process.WaitForExit();
+                    txtConsole.AppendText(process.StandardOutput.ReadToEnd());
+                }
+                catch (IOException iox)
+                {
+                    txtConsole.AppendText(iox.Message);
+                }
+
+                MessageBox.Show("DONE");
+            }
+            catch (IOException iox)
+            {
+                txtConsole.AppendText(iox.Message);
+            }
+        }
+    }
+}
